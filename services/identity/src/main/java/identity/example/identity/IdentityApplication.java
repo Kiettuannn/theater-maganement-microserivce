733609debac52b9a3611716264a283d543d4cda8
KietTuan <theonlytruth25012005@gmail.com>
2026-05-20 22:16:26 +0700

    add dependencies, add docker compose

--- services/identity/src/main/java/identity/example/identity/IdentityApplication.java
+++ services/identity/src/main/java/identity/example/identity/IdentityApplication.java
@@ -3,11 +3,15 @@ package identity.example.identity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class IdentityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(IdentityApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+
+        SpringApplication.run(IdentityApplication.class, args);
 	}
 
 }
