48db2ded62c0c05e301a5b28287f32010cda2872
KietTuan <theonlytruth25012005@gmail.com>
2026-05-21 16:16:11 +0700

    add full controller

--- services/identity/src/main/java/identity/example/identity/IdentityApplication.java
+++ services/identity/src/main/java/identity/example/identity/IdentityApplication.java
@@ -2,10 +2,14 @@ package identity.example.identity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableFeignClients
 public class IdentityApplication {
 
 	public static void main(String[] args) {
---
+++ services/identity/src/main/java/identity/example/identity/controller/AuthenticationController.java
@@ -0,0 +1,54 @@
+package identity.example.identity.controller;
+
+import com.nimbusds.jose.JOSEException;
+import identity.example.identity.dto.request.AuthenticationRequest;
+import identity.example.identity.dto.request.IntrospectRequest;
+import identity.example.identity.dto.request.LogoutRequest;
+import identity.example.identity.dto.request.RefreshTokenRequest;
+import identity.example.identity.dto.response.ApiResponse;
+import identity.example.identity.dto.response.AuthenticationResponse;
+import identity.example.identity.dto.response.IntrospectResponse;
+import identity.example.identity.service.AuthenticationService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class AuthenticationController {
+    AuthenticationService authenticationService;
+
+    @PostMapping("/token")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody AuthenticationRequest request) {
+        var result = authenticationService.authenticate(request);
+        return ApiResponse.<AuthenticationResponse>builder().result(result).build();
+    }
+
+    @PostMapping("/introspect")
+    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request)
+            throws ParseException, JOSEException {
+        var result = authenticationService.introspect(request);
+        return ApiResponse.<IntrospectResponse>builder().result(result).build();
+    }
+
+    @PostMapping("/logout")
+    ApiResponse<Void> authenticate(@RequestBody LogoutRequest request) throws ParseException, JOSEException {
+        authenticationService.logout(request);
+        return ApiResponse.<Void>builder().build();
+    }
+
+    @PostMapping("/refresh")
+    ApiResponse<AuthenticationResponse> authenticate(@RequestBody RefreshTokenRequest request)
+            throws ParseException, JOSEException {
+        var result = authenticationService.refreshToken(request);
+        return ApiResponse.<AuthenticationResponse>builder().result(result).build();
+    }
+}
